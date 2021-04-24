0948f5a66e37ced75366fa53640aa6a73071dc14
Ye Xiao <xiaoye.xy127@gmail.com>
2021-04-24 17:41:05 -0400

    add cors policy

--- backend/src/main/java/edu/gatech/streamingwars/rest/TestController.java
+++ backend/src/main/java/edu/gatech/streamingwars/rest/TestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
     private DemographicGroupDAO demographicGroupDAO;
 
