0d80352e6e316ff7627f8a9ba136d4e189c98542
Ye Xiao <xiaoye.xy127@gmail.com>
2021-04-24 15:39:39 -0700

    FE: connect to BE and finish display action

--- backend/src/main/java/edu/gatech/streamingwars/rest/ApiController.java
+++ backend/src/main/java/edu/gatech/streamingwars/rest/ApiController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApiController {
     private ShowLicenseManagementSystem showLicenseManagementSystem;
     private StreamingServiceSystem streamingServiceSystem;
